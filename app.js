259fd325694e7a548aac43be461e1ea2368bac9c
Max Antonucci <maxx1128@gmail.com>
2017-05-02 13:30:53 -0400

    Initial commit, liquid templating

---
+++ app.js
@@ -0,0 +1,40 @@
+var
+  http       = require("http"),
+  path       = require("path"),
+  express    = require("express"),
+  logger     = require("morgan"),
+  liquid     = require("shopify-liquid"),
+  bodyParser = require("body-parser")
+;
+
+var engine = liquid({
+    root: __dirname,  // for layouts and partials
+    extname: '.liquid'
+});
+
+var app = express();
+
+app.engine('liquid', engine.express()); 
+app.set('views', ['./views', './views/partials', './views/layouts']); 
+app.set('view engine', 'liquid');
+
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get("/", function(request, response) {
+  response.render("index", {
+    title: "Express Skeleton"
+  });
+});
+
+app.use(function(request, response) {
+  response.status(404).render("404");
+});
+
+http.createServer(app).listen(3000, function(){
+  console.log('App skeleton started on port 3000.');
+});
+
